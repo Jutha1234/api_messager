987c87d8a52af8a5923f7850873de77a4f3ec3f1
Jutha1234 <Juthathip.Che@gmail.com>
2018-09-20 11:17:32 +0700

    update appid

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $fb = new \Facebook\Facebook([
   'app_id' => '2183115115305945',
   'app_secret' => '0e5c2f9fd1626689fb67a5df4dead4e2',
   'default_graph_version' => 'v3.1',
-  //'default_access_token' => '{access-token}', // optional
+  'default_access_token' => 'EAAfBiBjzm9kBAOw4A4L18GAFv3ZAaSV5nwtwZBQzhXBT30FKzLn2a0JVqUi7F6DTzuLItZBL97zIHyR9JD2ZBNZCchUf61yrovuLyJkhBxPPvK3VLOoKKXcK6IwZBgHwAvfnAvM9eZAwZC6bkZAWlvaJX49gYZChrDvSEacqm68cKIQQZDZD', // optional
 ]);
 
 echo "start ...........";
