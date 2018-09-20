7d908c535bb7e3848da06fccbe2f3dc2e0348b51
Jutha1234 <Juthathip.Che@gmail.com>
2018-09-20 11:04:27 +0700

    update appid

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    'verify_token' => '123214qwerasdf',
+    'verify_token' => '1234qwerasdf',
     'token' => 'EAAfBiBjzm9kBAOw4A4L18GAFv3ZAaSV5nwtwZBQzhXBT30FKzLn2a0JVqUi7F6DTzuLItZBL97zIHyR9JD2ZBNZCchUf61yrovuLyJkhBxPPvK3VLOoKKXcK6IwZBgHwAvfnAvM9eZAwZC6bkZAWlvaJX49gYZChrDvSEacqm68cKIQQZDZD'
 ];
