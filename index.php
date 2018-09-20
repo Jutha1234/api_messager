f7cbc3b5835ad9b383371f9cd7c85025637dfec0
Jutha1234 <Juthathip.Che@gmail.com>
2018-09-20 18:19:22 +0700

    update  api

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php
 session_start();
-require_once $_SERVER["DOCUMENT_ROOT"] . '/vendor/autoload.php'; // change path as needed
-
-$fb = new \Facebook\Facebook([
-  'app_id' => '229194961285252',
-  'app_secret' => 'b54ce75c34a6d71843a9b88f2734f610',
-  'default_graph_version' => 'v3.1',
-  //'default_access_token' => '{access-token}', // optional
-]);
-
-echo '\r\nhttp://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 
 $url = $_SERVER['REQUEST_URI']; //returns the current URL
 $parts = explode('/',$url);
@@ -19,9 +9,24 @@ for ($i = 0; $i < count($parts) - 1; $i++) {
 }
 echo $dir;
 
-echo "\r\n------------";
+echo "<br>";
 
-echo '\r\n'.$fb;
+require_once $dir . '/vendor/autoload.php'; // change path as needed
+
+$fb = new \Facebook\Facebook([
+  'app_id' => '229194961285252',
+  'app_secret' => 'b54ce75c34a6d71843a9b88f2734f610',
+  'default_graph_version' => 'v3.1',
+  //'default_access_token' => '{access-token}', // optional
+]);
+
+echo 'http://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+echo "<br>";
+
+
+echo "<br>";
+
+echo $fb;
 
 $helper = $fb->getRedirectLoginHelper();
 $permissions = ['email']; // Optional permissions
