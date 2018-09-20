b1e8dbc06ee41d82e458905ef194fc554845ff85
Jutha1234 <Juthathip.Che@gmail.com>
2018-09-20 10:29:32 +0700

    edit config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 return [
     'verify_token' => '1234qwerasdf',
-    'token' => 'EAAfBiBjzm9kBAOKstiT8T3ZBP6UNvaRkXMaPhKfIfTfj7KHRmW1EW6eTAQZCcWOR4CxbViONBV35aZA1jNkqY3CfJlxpX5kV2xXALgjpMdVZBKRarxiMW2fZBwWT4bTRfFg23ytj6XSQ7b7dx8ZA8lwXPP4IMzmUw5qh8m9aC3nAZDZD'
+    'token' => 'EAAfBiBjzm9kBAAE2GO00O3rnuwn1BNSRvZBLpkv45qmrObnZB8xdsYhzIdMXGmZANtV9xh4eCJ156debusktS20e5v3ry07ysRmiDExBVyjZAdxlD4rOwBEEkeJcvFTZCavLUZCZBXcP4BZAsZAm0eIDaO0EH4BdyIie09ZBlnqFMlZCwZDZD'
 ];
