335de6e49efd22f13e0bc3e6ee0a0681aa4f53ef
Jutha1234 <Juthathip.Che@gmail.com>
2018-09-21 10:31:21 +0700

    update  api

--- page.php
+++ page.php
@@ -3,8 +3,8 @@ session_start();
 require_once __DIR__ . '/vendor/autoload.php'; // change path as needed
 
 $fb = new \Facebook\Facebook([
-  'app_id' => '{your-app-id}',
-  'app_secret' => '{your-secret-id}',
+  'app_id' => '229194961285252',
+  'app_secret' => 'b54ce75c34a6d71843a9b88f2734f610',
   'default_graph_version' => 'v3.1',
   //'default_access_token' => '{access-token}', // optional
 ]);
